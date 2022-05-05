3318a7bf9890648bc4ddd5df45b1e87e7d7a6a86
4Mago <47850942+4Mago@users.noreply.github.com>
2022-05-05 23:00:34 +0200

    Add files via upload

--- partybox.js
+++ partybox.js
@@ -8,10 +8,3 @@
 
 
 
-async function getData() {
-    let trafiklab = '24e8694550204651b656e322387bf0bc'
-	const resGet = await axios.get("https://api.sl.se/api2/realtimedeparturesV4.xml?key=24e8694550204651b656e322387bf0bc&siteid=1365&timewindow=5");
-	console.log('data :>> ', resGet);
-	return resGet
-};
-getData()
